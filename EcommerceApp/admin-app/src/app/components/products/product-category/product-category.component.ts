4a86cb45fb80cb384e2156bff6f58dda629d41b8
Brandon McKay <brandonmckay07@gmail.com>
2022-08-15 16:04:38 -0700

    new comit

--- EcommerceApp/admin-app/src/app/components/products/product-category/product-category.component.ts
+++ EcommerceApp/admin-app/src/app/components/products/product-category/product-category.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
+import { CategoriesService } from 'src/app/services/categories.service';
 @Component({
   selector: 'app-product-category',
   templateUrl: './product-category.component.html',
@@ -8,9 +9,12 @@ import { Observable } from 'rxjs';
 export class ProductCategoryComponent implements OnInit {
 
   public categoryObservable: Observable<any[]> = new Observable();
-  constructor() { }
+  
+
+  constructor(private categoriesService:CategoriesService) { }
 
   ngOnInit(): void {
+    this.categoryObservable = this.categoriesService.getCategories();
   }
 
 }
